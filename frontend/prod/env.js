70a18152c39a6e20c0d4fc7dbedaf70dc27a4408
Pablo Fraile Alonso <pablo@pablofraile.net>
2024-12-09 13:40:21 +0100

    feat(frontend): add cloudflare tunnel url

--- frontend/prod/env.js
+++ frontend/prod/env.js
@@ -1,5 +1,5 @@
 window.env = {
   SPOTIFY_CLIENT_ID: "f3d641d567484158967a7832a6b8d418",
   FRONTEND_URL: "https://pablito2020.github.io/anti-recommender",
-  BACKEND_URL: "http://localhost:8000",
+  BACKEND_URL: "https://epic-talks-alt-enhance.trycloudflare.com",
 };
